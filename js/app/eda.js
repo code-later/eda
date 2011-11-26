5813af22991457f11b7c5ac638d70717676143a9
Dirk Breuer <dirk.breuer@gmail.com>
2011-11-26 01:13:42 +0100

    added issue list entry view and collection

--- js/app/eda.js
+++ js/app/eda.js
@@ -24,7 +24,45 @@
       }
     });
 
+    window.IssueListEntryView = Backbone.View.extend({
+      template: _.template($("#issue-list-entry-template").html()),
+      tag: "div",
+      className: "issue",
+
+      initialize: function() {
+        _.bindAll(this, 'render');
+      },
+
+      render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+      }
+    });
+
     window.IssueListView = Backbone.View.extend({
+      template: _.template($("#issue-list-template").html()),
+      tag: "div",
+      className: "issue list",
+
+      initialize: function() {
+        _.bindAll(this, 'render',
+                  'renderIssue');
+      },
+
+      render: function() {
+        this.collection.each(this.renderIssue);
+
+        return this;
+      },
+
+      renderIssue: function(issue) {
+        console.log("render an issue in the list", issue);
+        console.log(this);
+        var view = new IssueListEntryView({
+          model: issue
+        });
+        $(this.el).append(view.render().el);
+      },
     });
 
   });
